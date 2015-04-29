676525c4eda36776aa30a59ed80911c2cbee87e7
PericoloMortale <marcoaguilar@Avas.local>
2015-04-29 14:49:10 +0200

    aggiornamento

--- js/saints-italia.js
+++ js/saints-italia.js
@@ -20,44 +20,60 @@ $(".navbar-header a[href^='#']").on('click', function(e) {
 
 });
 
-  $(window).scroll(function () {
-        //if you hard code, then use console
-        //.log to determine when you want the 
-        //nav bar to stick.  
-        console.log($(window).scrollTop())
-      if ($(window).scrollTop() > 150) {
-        $('#saints-italia').addClass('navbar-fixed');
-      }
-      if ($(window).scrollTop() < 149	) {
-        $('#saints-italia').removeClass('navbar-fixed');
-      }
-    });
+$(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 150) {
+      $('#saints-italia').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 149	) {
+      $('#saints-italia').removeClass('navbar-fixed');
+    }
+  });
 
-  $(window).scroll(function () {
-        //if you hard code, then use console
-        //.log to determine when you want the 
-        //nav bar to stick.  
-        console.log($(window).scrollTop())
-      if ($(window).scrollTop() > 150) {
-        $('.navbar-static-top-b').addClass('navbar-fixed-b');
-      }
-      if ($(window).scrollTop() < 149) {
-        $('.navbar-static-top-b').removeClass('navbar-fixed-b');
-      }
-    });
+$(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 150) {
+      $('.navbar-static-top-b').addClass('navbar-fixed-b');
+    }
+    if ($(window).scrollTop() < 149) {
+      $('.navbar-static-top-b').removeClass('navbar-fixed-b');
+    }
+  });
 
 
-  $(window).scroll(function () {
-        //if you hard code, then use console
-        //.log to determine when you want the 
-        //nav bar to stick.  
-        console.log($(window).scrollTop())
-      if ($(window).scrollTop() > 150) {
-        $('#immagine-sfondo').addClass('immagine-sfondo-b');
-      }
-     
-      if ($(window).scrollTop() < 149) {
-        $('#immagine-sfondo').removeClass('immagine-sfondo-b');
-      }
-    });
+$(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 150) {
+      $('#immagine-sfondo').addClass('immagine-sfondo-b');
+    }
+   
+    if ($(window).scrollTop() < 149) {
+      $('#immagine-sfondo').removeClass('immagine-sfondo-b');
+    }
+  });
+
+$(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 150) {
+      $('.twitter-box').addClass('twitter-fixed');
+    }
+   
+    if ($(window).scrollTop() < 149) {
+      $('.twitter-box').removeClass('twitter-fixed');
+    }
+  });
+
+
 
