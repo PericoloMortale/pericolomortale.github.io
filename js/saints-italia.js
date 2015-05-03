024ea33c6ea695aa03488a5f33466a386bb11a15
PericoloMortale <studioaguilar@iMac-di-Studio.local>
2015-05-03 23:03:57 +0200

    a

--- js/saints-italia.js
+++ js/saints-italia.js
@@ -61,7 +61,7 @@ $(window).scroll(function () {
     }
   });
 
-/*
+
 $(window).scroll(function () {
       //if you hard code, then use console
       //.log to determine when you want the 
@@ -74,7 +74,7 @@ $(window).scroll(function () {
     if ($(window).scrollTop() < 149) {
       $('.twitter-box').removeClass('twitter-fixed');
     }
-  }); */
+  });
 
 
 
